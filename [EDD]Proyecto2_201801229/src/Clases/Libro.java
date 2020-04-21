742f074c7bcf1560b91891669eb8c9ffd4d5028a
OsmanPL <60387600+OsmanPL@users.noreply.github.com>
2020-04-21 10:37:55 -0600

    Creacion Proyecto 2

---
+++ [EDD]Proyecto2_201801229/src/Clases/Libro.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author l4kz4
+ */
+public class Libro {
+    private int ISBN, carnetUsuario, año;
+    private String autor, titulo, editorial, edicion, categoria, idioma;
+
+    public Libro() {
+    }
+
+    public Libro(int ISBN, int carnetUsuario, int año, String autor, String titulo, String editorial, String edicion, String categoria, String idioma) {
+        this.ISBN = ISBN;
+        this.carnetUsuario = carnetUsuario;
+        this.año = año;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.editorial = editorial;
+        this.edicion = edicion;
+        this.categoria = categoria;
+        this.idioma = idioma;
+    }
+
+    /**
+     * @return the ISBN
+     */
+    public int getISBN() {
+        return ISBN;
+    }
+
+    /**
+     * @param ISBN the ISBN to set
+     */
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    /**
+     * @return the carnetUsuario
+     */
+    public int getCarnetUsuario() {
+        return carnetUsuario;
+    }
+
+    /**
+     * @param carnetUsuario the carnetUsuario to set
+     */
+    public void setCarnetUsuario(int carnetUsuario) {
+        this.carnetUsuario = carnetUsuario;
+    }
+
+    /**
+     * @return the año
+     */
+    public int getAño() {
+        return año;
+    }
+
+    /**
+     * @param año the año to set
+     */
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    /**
+     * @return the autor
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the editorial
+     */
+    public String getEditorial() {
+        return editorial;
+    }
+
+    /**
+     * @param editorial the editorial to set
+     */
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    /**
+     * @return the edicion
+     */
+    public String getEdicion() {
+        return edicion;
+    }
+
+    /**
+     * @param edicion the edicion to set
+     */
+    public void setEdicion(String edicion) {
+        this.edicion = edicion;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the idioma
+     */
+    public String getIdioma() {
+        return idioma;
+    }
+
+    /**
+     * @param idioma the idioma to set
+     */
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+}
---
+++ [EDD]Proyecto2_201801229/src/edd/proyecto2_201801229/EDDProyecto2_201801229.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd.proyecto2_201801229;
+
+/**
+ *
+ * @author l4kz4
+ */
+public class EDDProyecto2_201801229 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
